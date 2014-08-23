c3f29158f680eb806da733ebd8a42917a150bf5d
Martin Bilski <martinb@criticue.com>
2014-08-23 19:47:51 +0200

    Initial revision.

---
+++ README.md
@@ -0,0 +1,44 @@
+## Getting and Cleaning Data Course Project
+
+### Introduction
+
+This is an introduction to a simple R script running basic cleanup and simplification of results of a research initiative performed at UCI.
+
+For more information see the original [README.txt](README.txt).
+
+### How to run
+
+Unfortunately, due to GitHub limations, this repository isn't self-contained, the train/ and test/ directories are deliberately empty and you have to follow this more cumbersome procedure:
+
+1. Download the data:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+2. Extract it to a folder.
+
+3. Copy the [run_analysis.R](run_analysis.R) script to the folder. (Either copy the Raw version of the script from this GitHub repository or clone it from the command line using `git clone https://github.com/bilus/getdata-005.git`.)
+
+4. Load the script into R Studio or run the script from command line. 
+
+*Note: When using R Studio, make sure to set working directory to the script's location (on OS/X, it's "Session" > "Set Working Directory" > "To Source File Location")*
+
+The result is in [clean_data.csv](clean_data.csv).
+
+### Analysis overview
+
+1. Read in feature and activity labels from "features.txt" and "activity_labels.txt".
+
+1. Read training and test data from the revelant files.
+
+1. Clean training & test data.
+
+- Combine the column containing the identifiers of subjects with columns holding the training data and activities. Name the new columns "Subject" and "Activity".
+- Make column labels into valid, descriptive variable names.
+- Select only the columns we're interested in: Subject, Activity, mean and standard deviation.
+
+1. Merge test & training data row-wise.
+
+1. Make column names user-friendly by converting dots to spaces. 
+
+1. Save the resulting data set to [clean_data.csv](clean_data.csv).
+
---
+++ run_analysis.R
@@ -0,0 +1,91 @@
+onlyInterestingColumns <- function(columnLabels) {
+  # We only want the subject id and mean and standard deviation columns.
+  columnLabels[grepl("(^Subject$)|(.mean)|(.std)|(Activity)", columnLabels)]
+}
+
+activityNames <- function(activities, activityCodes) {
+  activities[activities$Code==activityCodes,]$Label
+}
+
+makeDescriptiveNames <- function(columnLabels) {
+  make.names(columnLabels)
+}
+
+makeColNamesUserFriendly <- function(ds) {
+  # FIXME: Repetitive.
+  
+  # Convert any number of consecutive dots to a single space.
+  names(ds) <- gsub(x = names(ds),
+                    pattern = "(\\.)+",
+                    replacement = " ")
+  
+  # Drop the trailing spaces.
+  names(ds) <- gsub(x = names(ds),
+                    pattern = "( )+$",
+                    replacement = "")
+  
+  ds
+}
+
+cleanData <- function(features, subjects, featureData, activities, activityData) {
+  # Combine the column containing the identifiers of subjects with columns holding the training data and activities.
+  normalizedData <- data.frame(subjects, featureData, lapply(activityData$V1, function(activityCode) activities[activities$Code==activityCode,]$Label ))
+  # Add labels for columns. 
+  # - Prepend the Subject column because we have added it as the first column.
+  columnLabels <- c("Subject", features, "Activity")
+  # - Make column labels into valid, descriptive variable names.
+  columnLabels <- makeDescriptiveNames(columnLabels)
+  # - Set column names in the training data frame.
+  colnames(normalizedData) <- columnLabels
+  # Select only the columns we're interested in.
+  normalizedData[onlyInterestingColumns(columnLabels)]
+}
+
+saveAverages <- function(mergedData, filename) {
+  renameColumn <- function(ds, from, to) {
+    names(ds)[names(ds) == from] <- to
+    ds
+  }
+  
+  averages <- aggregate(mergedData, by=list(mergedData$Subject, mergedData$Activity), FUN=mean)
+  cols <- colnames(averages)[cols != "Activity" & cols != "Subject"]
+  averages <- averages[, cols]
+  averages <- renameColumn(averages, "Group.1", "Subject")
+  averages <- renameColumn(averages, "Group.2", "Activity")
+  averages <- makeColNamesUserFriendly(averages)
+  write.table(averages, file=filename, row.names=FALSE)
+}
+
+# Read in feature labels. Do not interpret the strings.
+features <- read.table("features.txt", as.is=TRUE)$V2
+# Read in activity labels. Do not interpret the strings.
+activities <- read.table("activity_labels.txt", as.is=TRUE)
+# Use intuitive column names.
+colnames(activities) <- c("Code", "Label")
+
+# Read training data & subjects.
+trainData <- read.table("train/X_train.txt", header=FALSE)
+trainSubjects <- read.table("train/subject_train.txt", header=FALSE)
+trainActivityData <- read.table("train/y_train.txt", header=FALSE)
+
+# Read test data & subjects.
+testData <- read.table("test/X_test.txt", header=FALSE)
+testSubjects <- read.table("test/subject_test.txt", header=FALSE)
+testActivityData <- read.table("test/y_test.txt", header=FALSE)
+
+# Clean training & test data.
+cleanTrainingData <- cleanData(features, trainSubjects, trainData, activities, trainActivityData)
+cleanTestData <- cleanData(features, testSubjects, testData, activities, testActivityData)
+
+# Merge test & training data row-wise.
+mergedData <- rbind(cleanTestData, cleanTrainingData)
+
+# Calculate averages by subject and activity. 
+# It's commented out because it's not in the requirements.
+# saveAverages(mergedData, "averages.txt")
+
+# Drop dots just before saving!
+mergedData <- makeColNamesUserFriendly(mergedData)
+
+# Save the resulting data set.
+write.csv(mergedData, file="clean_data.csv", row.names=FALSE)
